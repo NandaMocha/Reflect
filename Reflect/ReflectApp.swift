cb6bc5aa332009f03bfdd844552184c9f3828a0e
Nanda Mochammad <nanda.mocha@gmail.com>
2026-01-27 13:11:48 +0700

    Initial Commit

---
+++ Reflect/ReflectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectApp.swift
+//  Reflect
+//
+//  Created by Nanda Mochammad on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ReflectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
